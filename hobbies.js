b10051c7b89b2a0a50055d4e8c9f60a18f76f264
Salvatore <workstudioms@gmail.com>
2025-01-31 16:10:49 +0100

    hobbies.js, tested and completed

--- hobbies.js
+++ hobbies.js
@@ -1,3 +1,16 @@
 // 2 - hobbies.js
-// 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
+// 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree.
+// La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
 // 2. Esporta la funzione dal file. 
+
+const getUserHobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+
+    const hobbies = [hobbyOne, hobbyTwo, hobbyThree];
+
+    return hobbies;
+
+}
+
+console.log(getUserHobbies('Bongo Drumming', 'Banana Cooking', 'Treasure Hunting'));
+
+module.exports = getUserHobbies;
