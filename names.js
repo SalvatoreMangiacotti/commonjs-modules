846b9e7b3d17f97b6a71f628e651e81f0a022a3f
Salvatore <workstudioms@gmail.com>
2025-01-31 16:04:09 +0100

    names.js function, tested and completed

--- names.js
+++ names.js
@@ -1,3 +1,21 @@
 // 1 - names.js
 // 1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 // 2. Esporta la funzione dal file.
+
+
+const getUserData = (firstName, lastName) => {
+
+    const user = {
+
+        name: firstName,
+        lastName: lastName
+
+    }
+
+    return user;
+
+}
+
+console.log(getUserData('Donkey', 'Kong'));
+
+module.exports = getUserData;
